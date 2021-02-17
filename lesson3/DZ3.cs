05617f30bea9a8c86cca2dec5cbee7cbcb80763d
Oleg <serv_all@mail.ru>
2021-02-17 16:27:04 +0300

    Выполнен шаблон для заполнения домашними заданиями(простое меню)

---
+++ lesson3/DZ3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace lesson3
+{
+    class DZ3
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            do
+            {
+                Console.WriteLine("Выберите пункт домашнего задания (цифры 1-4, q-для выхода)");
+                userInput = Console.ReadLine().ToLower();
+                switch (userInput)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine($"Задание {userInput}");
+                        }
+                        break;
+                    case "2":
+                        {
+                            Console.WriteLine($"Задание {userInput}");
+                        }
+                        break;
+                    case "3":
+                        {
+                            Console.WriteLine($"Задание {userInput}");
+                        }
+                        break;
+                    case "4":
+                        {
+                            Console.WriteLine($"Задание {userInput}");
+                        }
+                        break;
+                    case "q":
+                        return;
+
+
+                    default:
+                        {
+                            Console.WriteLine("Введено что-то непонятное, повторите ввод");
+                        }
+                        break;
+                }
+
+
+
+
+
+
+            } while (true);
+
+
+
+
+        }
+    }
+}
