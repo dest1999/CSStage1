037ba28bab24dd0a0a5a62c2d43dee7db90ac22f
Oleg <serv_all@mail.ru>
2021-02-18 17:45:56 +0300

    Выполнен вывод диагоналей массива

--- lesson3/DZ3.cs
+++ lesson3/DZ3.cs
@@ -6,6 +6,7 @@ namespace lesson3
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random(); //это для заполнения массива
             string userInput;
             do
             {
@@ -13,22 +14,59 @@ namespace lesson3
                 userInput = Console.ReadLine().ToLower();
                 switch (userInput)
                 {
-                    case "1":
+                    case "1":   //TODO вывести диагональ двумерного массива
+                        {
+                            Console.Clear();
+                            uint arrayLength;
+                            do
+                            {
+                                Console.Write("Введите размер квадратного массива: ");
+                            } while (!uint.TryParse(Console.ReadLine(), out arrayLength));
+                            int[,] array = new int[arrayLength, arrayLength];
+                            //в этом цикле заполняем массив случайными значениями
+                            for (int i = 0; i < array.GetLength(0); i++)
+                            {
+                                for (int j = 0; j < array.GetLength(1); j++)
+                                {
+                                    array[i, j] = rnd.Next(10, 100);
+                                    if ((i == j) || (j == (array.GetLength(1) - i - 1)) )
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.Green;
+                                        Console.Write(array[i,j] + " ");
+                                        Console.ResetColor();
+                                    }
+                                    else
+                                    {
+                                        Console.Write(array[i, j] + " ");
+                                    }
+                                }
+                                Console.WriteLine();
+                            }
+                            Console.WriteLine("\nПервая диагональ:");
+                            for (int i = 0; i < array.GetLength(0); i++)
+                            {
+                                Console.Write(array[i,i] + " ");
+                            }
+
+                            Console.WriteLine("\nВторая диагональ:");
+                            for (int i = array.GetLength(0)-1; i >= 0; i--)
+                            {
+                                Console.Write( array[i, array.GetLength(1) - i - 1] + " " );
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    case "2":   //TODO Телефонный справочник: массив 5х2
                         {
                             Console.WriteLine($"Задание {userInput}");
                         }
                         break;
-                    case "2":
+                    case "3":   //TODO Написать программу, выводящую введённую пользователем строку в обратном порядке
                         {
                             Console.WriteLine($"Задание {userInput}");
                         }
                         break;
-                    case "3":
-                        {
-                            Console.WriteLine($"Задание {userInput}");
-                        }
-                        break;
-                    case "4":
+                    case "4":   //TODO *«Морской бой»: вывести на экран массив 10х10, состоящий из символов X и O, где Х — элементы кораблей, а О — свободные клетки
                         {
                             Console.WriteLine($"Задание {userInput}");
                         }
