465b96cfb964cfb524b1efcd9f1347594db30ed5
Oleg <serv_all@mail.ru>
2021-02-25 19:55:21 +0300

    Задание 3 выполнено (методы определения времени года)

--- lesson4/DZ4.cs
+++ lesson4/DZ4.cs
@@ -4,6 +4,13 @@ namespace lesson3
 {
     class DZ4
     {
+        enum Season //перечисление к заданию 3
+        {
+            winter,
+            spring,
+            summer,
+            autumn
+        }
 
         static void Main(string[] args)
         {
@@ -22,6 +29,67 @@ namespace lesson3
                     res += tmp;
                 }
                 return res;
+            }
+
+            Season? SetSeason(int monthNumber) //метод к заданию 3
+            {   // Вопрос по выполнению: возврат nullable норм? или лучше так не делать?
+                //                       использовать лучше if или switch?
+                switch (monthNumber)
+                {
+                    case 1:
+                    case 2:
+                    case 12:
+                        {
+                            return Season.winter;
+                        }
+                    case 3:
+                    case 4:
+                    case 5:
+                        {
+                            return Season.spring;
+                        }
+                    case 6:
+                    case 7:
+                    case 8:
+                        {
+                            return Season.summer;
+                        }
+                    case 9:
+                    case 10:
+                    case 11:
+                        {
+                            return Season.autumn;
+                        }
+                    default:
+                        return null;
+                }
+            }
+
+            string ShowSeason(Season? season) //еще метод к заданию 3
+            {
+                switch (season)
+                {
+                    case Season.winter:
+                        {
+                            return "Зима";
+                        }
+                    case Season.spring:
+                        {
+                            return "Весна";
+                        }
+                    case Season.summer:
+                        {
+                            return "Лето";
+                        }
+                    case Season.autumn:
+                        {
+                            return "Осень";
+                        }
+                    default:
+                        {
+                            return "Ошибка: введите число от 1 до 12";
+                        }
+                }
             }
 
             string userInput;
@@ -58,6 +126,14 @@ namespace lesson3
                     case "3":
                         {// TODO Написать метод по определению времени года. На вход подаётся число – порядковый номер месяца. На выходе — значение из перечисления (enum) — Winter, Spring, Summer, Autumn
                             Console.Clear();
+                            int number;
+                            do
+                            {
+                                Console.Write("Введите порядковый номер месяца: ");
+
+                            } while (!int.TryParse(Console.ReadLine(), out number));
+
+                            Console.WriteLine( ShowSeason (SetSeason(number)));
 
                         }
                         break;
