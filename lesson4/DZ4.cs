f68235e18f332ae4932f2177821eab6854f00388
Oleg <serv_all@mail.ru>
2021-02-26 00:19:45 +0300

    Задание 4 выполнено

--- lesson4/DZ4.cs
+++ lesson4/DZ4.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace lesson3
+namespace lesson4
 {
     class DZ4
     {
@@ -12,85 +12,85 @@ namespace lesson3
             autumn
         }
 
+        static string GetFullName(string firstName, string lastName, string patronymic) // метод к заданию 1
+        {
+            return $"{lastName} {firstName} {patronymic}";
+        }
+
+        static int Sum(string str) //метод к заданию 2
+        {
+            string[] strArray = str.Split(' ');
+            int res = 0;
+            foreach (var item in strArray)
+            {
+                int.TryParse(item, out int tmp);
+                res += tmp;
+            }
+            return res;
+        }
+
+        static Season? SetSeason(int monthNumber) //метод к заданию 3
+        {   // Вопрос по выполнению: возврат nullable норм или лучше так не делать?
+            //                       в данном случае использовать лучше if или switch?
+            switch (monthNumber)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    {
+                        return Season.winter;
+                    }
+                case 3:
+                case 4:
+                case 5:
+                    {
+                        return Season.spring;
+                    }
+                case 6:
+                case 7:
+                case 8:
+                    {
+                        return Season.summer;
+                    }
+                case 9:
+                case 10:
+                case 11:
+                    {
+                        return Season.autumn;
+                    }
+                default:
+                    return null;
+            }
+        }
+
+        static string ShowSeason(Season? season) //еще метод к заданию 3
+        {
+            switch (season)
+            {
+                case Season.winter:
+                    {
+                        return "Зима";
+                    }
+                case Season.spring:
+                    {
+                        return "Весна";
+                    }
+                case Season.summer:
+                    {
+                        return "Лето";
+                    }
+                case Season.autumn:
+                    {
+                        return "Осень";
+                    }
+                default:
+                    {
+                        return "Ошибка: введите число от 1 до 12";
+                    }
+            }
+        }
         static void Main(string[] args)
         {
-            string GetFullName(string firstName, string lastName, string patronymic) // метод к заданию 1
-            {
-                return $"{lastName} {firstName} {patronymic}";
-            }
-
-            int Sum(string str) //метод к заданию 2
-            {
-                string[] strArray = str.Split(' ');
-                int res = 0;
-                foreach (var item in strArray)
-                {
-                    int.TryParse(item, out int tmp);
-                    res += tmp;
-                }
-                return res;
-            }
-
-            Season? SetSeason(int monthNumber) //метод к заданию 3
-            {   // Вопрос по выполнению: возврат nullable норм? или лучше так не делать?
-                //                       использовать лучше if или switch?
-                switch (monthNumber)
-                {
-                    case 1:
-                    case 2:
-                    case 12:
-                        {
-                            return Season.winter;
-                        }
-                    case 3:
-                    case 4:
-                    case 5:
-                        {
-                            return Season.spring;
-                        }
-                    case 6:
-                    case 7:
-                    case 8:
-                        {
-                            return Season.summer;
-                        }
-                    case 9:
-                    case 10:
-                    case 11:
-                        {
-                            return Season.autumn;
-                        }
-                    default:
-                        return null;
-                }
-            }
-
-            string ShowSeason(Season? season) //еще метод к заданию 3
-            {
-                switch (season)
-                {
-                    case Season.winter:
-                        {
-                            return "Зима";
-                        }
-                    case Season.spring:
-                        {
-                            return "Весна";
-                        }
-                    case Season.summer:
-                        {
-                            return "Лето";
-                        }
-                    case Season.autumn:
-                        {
-                            return "Осень";
-                        }
-                    default:
-                        {
-                            return "Ошибка: введите число от 1 до 12";
-                        }
-                }
-            }
 
             string userInput;
             do
@@ -130,16 +130,21 @@ namespace lesson3
                             do
                             {
                                 Console.Write("Введите порядковый номер месяца: ");
-
                             } while (!int.TryParse(Console.ReadLine(), out number));
 
                             Console.WriteLine( ShowSeason (SetSeason(number)));
-
                         }
                         break;
                     case "4":
                         {// TODO (*) Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом
                             Console.Clear();
+                            uint n;
+                            do
+                            {
+                                Console.WriteLine("Введите положительное число для вычисления ряда Фибоначчи: ");
+                            } while (!uint.TryParse(Console.ReadLine(), out n));
+
+                            Console.WriteLine(FibonacciSeq(n));
 
                         }
                         break;
@@ -151,6 +156,14 @@ namespace lesson3
                         break;
                 }
             } while (userInput != "exit");
+        }
+
+        static uint FibonacciSeq(uint n) //к заданию 4, рекурсивный метод
+        {
+            if (n == 0)
+                return n;
+
+            return n + FibonacciSeq(n - 1);
         }
     }
 }
